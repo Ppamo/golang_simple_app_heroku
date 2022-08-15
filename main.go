ed311f6718dc6b686db2d06d279475eca9bc1269
Ppamo <pablo@ppamo.cl>
2022-08-15 15:46:08 -0400

    Add FLAVOUR env var

--- main.go
+++ main.go
@@ -9,13 +9,14 @@ import (
 
 var (
 	SERVER_PORT=os.Getenv("PORT")
+	FLAVOUR=os.Getenv("FLAVOUR")
 )
 
 func main() {
 	e := echo.New()
 	e.GET("/ping", func(c echo.Context) error{
 		fmt.Printf("> ping\n")
-		return c.String(http.StatusOK, "{\"status\":\"pong\"}")
+		return c.String(http.StatusOK, "{\"status\":\"pong\", \"flavour\": \"" + FLAVOUR + "\"}")
 	})
 	e.Logger.Fatal(e.Start(":" + SERVER_PORT))
 }
